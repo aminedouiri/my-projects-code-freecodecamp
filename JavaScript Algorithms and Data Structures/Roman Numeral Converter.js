db8a839c2a4a335c2c9e0cb5303b22ab46ccf644
aminedouiri <aminedouiri2018@gmail.com>
2022-10-09 00:54:45 +0100

    ADD Roman Numeral Converter

---
+++ JavaScript Algorithms and Data Structures/Roman Numeral Converter.js
@@ -0,0 +1,27 @@
+function convertToRoman() {
+    let numero = document.getElementById('romanNumber').value;
+    const numerals = {
+        1: 'I',
+        4: 'IV',
+        5: 'V',
+        9: 'IX',
+        10: 'X',
+        40: 'XL',
+        50: 'L',
+        90: 'XC',
+        100: 'C',
+        400: 'CD',
+        500: 'D',
+        900: 'CM',
+        1000: 'M'
+    };
+    let roman_text = '';
+    const decimalKeys = Object.keys(numerals).reverse();
+    decimalKeys.forEach(key => {
+        while (key <= numero) {
+            roman_text += numerals[key];
+            numero -= key;
+        }
+    });
+    document.getElementById('romanResultat').textContent = roman_text;
+}
